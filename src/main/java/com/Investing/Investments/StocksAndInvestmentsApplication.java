f29752a9696c55ec8cdf80948867f5dbfbba72fd
John <jvt703@gmail.com>
2023-07-10 14:34:40 -0400

    Initial commit

---
+++ src/main/java/com/Investing/Investments/StocksAndInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package com.Investing.Investments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksAndInvestmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksAndInvestmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Investing/Investments/StocksAndInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Investing.Investments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksAndInvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
